8af74246aef6c7b5b8ad243417fa4ddfbf41354a
Fernando Contreras <fernandomorann999@gmail.com>
2023-02-14 09:22:37 -0600

    Serie V y Serie VI
    Actualizamos el archivo main.py y agregamos un archivo .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("FERNANDO DAVID CONTRERAS MORAN, CARNÉ 1631023, INTRODUCCIÓN A LA INGENIERIA EN INFORMÁTICA Y SISTEMAS")
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt','w')
+    archivo.write('Hola mundo')
+    archivo.close()
